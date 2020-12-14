e01048b4ec0b55af6b8ae686ac9589cb78ac0cdd
e9871003 <honghsp@hanafn.com>
2020-12-14 18:05:49 +0900

    화면 frame/ css 수정/ jquery 스크립트 추가

--- src/webpack/organic-animal-project/src/ORGANI/vues/MAN/MAN-01-001.vue
+++ src/webpack/organic-animal-project/src/ORGANI/vues/MAN/MAN-01-001.vue
@@ -23,7 +23,56 @@
             <h1>OAP</h1>
             <button type="button" class="header_btn" @click="testClose()"><span>닫기</span></button>
         </div>
+        <div class="contents" style="width:100%; height:100%">
+            <div clase="infoClass" style="width: 100%; height: 110px;">
+                <img style="width: 100px;height: 100px; position: absolute; left: 10px" />
+                <div style="position: absolute; padding-left: 114px;"> 
+                    <span style="width: 100%; display: block;">이름 : </span>
+                    <span style="width: 100%; display: block;">나이 : </span>
+                    <span style="width: 100%; display: block;">성별 : </span>
+                    <span style="width: 100%; display: block;">주소 : </span>
+                </div>
+            </div>
+            <div clase="infoClass" style="width: 100%; height: 110px;">
+                <img style="width: 100px;height: 100px; position: absolute; left: 10px" />
+                <div style="position: absolute; padding-left: 114px;"> 
+                    <span style="width: 100%; display: block;">이름 : </span>
+                    <span style="width: 100%; display: block;">나이 : </span>
+                    <span style="width: 100%; display: block;">성별 : </span>
+                    <span style="width: 100%; display: block;">주소 : </span>
+                </div>
+            </div>
+            <div clase="infoClass" style="width: 100%; height: 110px;">
+                <img style="width: 100px;height: 100px; position: absolute; left: 10px" />
+                <div style="position: absolute; padding-left: 114px;"> 
+                    <span style="width: 100%; display: block;">이름 : </span>
+                    <span style="width: 100%; display: block;">나이 : </span>
+                    <span style="width: 100%; display: block;">성별 : </span>
+                    <span style="width: 100%; display: block;">주소 : </span>
+                </div>
+            </div>
+            <div clase="infoClass" style="width: 100%; height: 110px;">
+                <img style="width: 100px;height: 100px; position: absolute; left: 10px" />
+                <div style="position: absolute; padding-left: 114px;"> 
+                    <span style="width: 100%; display: block;">이름 : </span>
+                    <span style="width: 100%; display: block;">나이 : </span>
+                    <span style="width: 100%; display: block;">성별 : </span>
+                    <span style="width: 100%; display: block;">주소 : </span>
+                </div>
+            </div>
+            <div clase="infoClass" style="width: 100%; height: 110px;">
+                <img style="width: 100px;height: 100px; position: absolute; left: 10px" />
+                <div style="position: absolute; padding-left: 114px;"> 
+                    <span style="width: 100%; display: block;">이름 : </span>
+                    <span style="width: 100%; display: block;">나이 : </span>
+                    <span style="width: 100%; display: block;">성별 : </span>
+                    <span style="width: 100%; display: block;">주소 : </span>
+                </div>
+            </div>
+        </div>
         <div :class="menuClass">
+            <div>
+            </div>
             <ul class="list">
                 <li class=""><a class="link_sub_item">메뉴1</a></li>
                 <li class=""><a class="link_sub_item">메뉴2</a></li>
@@ -34,6 +83,7 @@
 <script>
 /* vuex 사용 */
 import { mapGetters } from 'vuex';
+import axios from 'axios'
 export default {
     name: 'MAN-01-001',
     components: {
@@ -51,7 +101,19 @@ export default {
 		
 	},
     created () {
-	 
+        // fetch('http://127.0.0.1:5000/kind/?up_kind_cd=417000').then((res)=>{
+        //     console.log('res ',res)
+        // })
+        //axios('http://127.0.0.1:5000/kind/?up_kind_cd=417000')
+        //jQuery.get('http://127.0.0.1:5000/kind/?up_kind_cd=417000')
+        // axios({
+        //     method:'GET',
+        //     url : 'http://127.0.0.1:5000/kind/?up_kind_cd=417000'
+        // }).then((res)=>{
+        //     console.log('res', res)
+        // }).catch(err =>{
+        //     console.log('err', err)
+        // })
     },
     methods: {
         // 햄버거 버튼 클릭
